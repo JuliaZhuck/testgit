95c3ec8d186924292b9cd6fbcfd6fca2bcb9ed6e
juliaavalach <juliaavalach@gmail.com>
2019-08-29 10:45:45 +0300

    add second string

--- testgit/Program.cs
+++ testgit/Program.cs
@@ -11,6 +11,7 @@ namespace testgit
         static void Main(string[] args)
         {
             Console.WriteLine("Test git hub");
+            Console.WriteLine("Add the second line");
         }
     }
 }
